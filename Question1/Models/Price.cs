107360dec4fdddcb368bc1b7a66fe7cadd0cc07b
duongtrong <trongdvd00543@fpt.edu.vn>
2019-01-08 21:30:50 +0700

    Nop Bai

---
+++ Question1/Models/Price.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Question1.Models
+{
+    public class Price
+    {
+        public int Id { get; set; }
+        public long Ratio { get; set; }
+    }
+}
